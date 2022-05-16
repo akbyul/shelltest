5cdb72682ebe13238cd1f02ae2d68d7457289c27
(kimmj2237) <(kimmj2237@gmail.com)>
2022-05-16 22:10:01 +0000

    ex04 login

--- ex04/login.sh
+++ ex04/login.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /etc/passwd | awk 'NR%2==0' | rev | sort -r | awk "NR >= $FT_LINE1 && NR <= $FT_LINE2" | tr '\n' ',' | sed -e "s/,$/.\n/"
+cat /etc/passwd | cut -d ':' -f 1 | awk 'NR%2==0' | rev | sort -r | awk "NR >= $FT_LINE1 && NR <= $FT_LINE2" | tr '\n' ',' | sed -e "s/,$/.\n/"
